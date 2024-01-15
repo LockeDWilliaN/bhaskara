b810246f3fd1429cefb06ae16194c1e66d44c10a
LockeDWilliaN <josewgodoypereira@gmail.com>
2024-01-15 13:27:00 -0300

    classe atualizada

--- bhaskara atualizado.py
+++ bhaskara atualizado.py
@@ -31,50 +31,39 @@ class Calculadora:
         if resultado is not None:
             delta, x1, x2, xv, yv = resultado
             print("\n--------------------------------")
-            print(f"Valor de Delta: {delta:.2f}")
+            print(f"Valor de Delta: {delta:.1f}")
         
             if delta == 0:
                 print("--------------------------------")
-                print(f"Valor de X: {x1:.2f}")
+                print(f"Valor de X: {x1:.1f}")
                 print("--------------------------------")
         
             if delta > 0:
                 print("--------------------------------")
-                print(f"Valor de X¹: {x1:.2f}")
-                print(f"Valor de X²: {x2:.2f}")
+                print(f"Valor de X¹: {x1:.1f}")
+                print(f"Valor de X²: {x2:.1f}")
                 print("--------------------------------")
                 
+
             if xv is not None and yv is not None:
-                print(f"Valor de Xvértice: {xv:.2f}")
-                print(f"Valor de Yvértice: {yv:.2f}")
-                print("--------------------------------\n")
+                print(f"Valor de Xvértice: {xv:.1f}")
+                print(f"Valor de Yvértice: {yv:.1f}")
+                print("--------------------------------")
 
         else:
-            delta = resultado
             print("\n--------------------------------")
-            print("A equação não possui raizes reais! (Delta < 0)")
-            print("--------------------------------\n")
+            print("ERRO: delta < 0")
+            print("--------------------------------")
             
+
+a = int(input("Digite o Valor de A: "))
+b = int(input("Digite o valor de B: "))
+c = int(input("Digite o valor de C: "))
     
 if __name__ == '__main__':
-    tentativas = 0
-    max_tentativas = 3
-    while tentativas < max_tentativas:
-        try:
-            a = int(input("Digite o Valor de A: "))
-            b = int(input("Digite o valor de B: "))
-            c = int(input("Digite o valor de C: ")) 
-            calc = Calculadora(a, b, c,)    
-            calc.results()
-            exit()
-        except ValueError:
-            tentativas += 1
-            if tentativas == max_tentativas:
-                sair = input("Deseja sair do programa? [s/n] ")
-                if sair.lower() == "s":
-                    print("Obrigado por utilizar o programa!")
-                    exit()
-                else:
-                    tentativas = 0
-            print("Por favor, digite um número\nTente Novamente")
-            
+    try:
+        calc = Calculadora(a, b, c,)    
+        calc.results()
+    except ValueError as e:
+        print(e)
+    
